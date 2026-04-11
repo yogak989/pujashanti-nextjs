d31909bf49aa5ff2100f65e1ef3e24e6821cccef
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-11 20:22:37 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Tambahkan baris di bawah ini untuk mematikan optimasi yang bikin error
+  optimizeFonts: false,
+  productionBrowserSourceMaps: false,
   images: {
     remotePatterns: [
       {
