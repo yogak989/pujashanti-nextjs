16676c62ef41cf34da3a0d48e53ca7a7927f92b0
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-11 18:45:06 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css';
 import Header from '../components/Header';
 
+// Di _app.js kita TIDAK perlu menambahkan 'export const runtime = edge'
+// Karena file ini hanya pembungkus (layout)
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <main className="container">
+      <main>
+        {/* Hapus className="container" jika Anda belum membuat CSS-nya agar tidak berantakan */}
         <Component {...pageProps} />
       </main>
     </>
