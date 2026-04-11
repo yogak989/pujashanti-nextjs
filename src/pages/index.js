3ca4f09a0bf061edd6fbf965cd0a2e82f6480dfd
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-11 10:04:04 +0800

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px 0' }}>
+      <h1>Selamat Datang di PujaShanti</h1>
+      <p>Situs utama sedang dalam pengembangan Headless.</p>
+    </div>
+  );
+}
