005f8915d55644a3aeea206f5a1fd15733dce2ff
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-11 21:12:02 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  // Kita matikan fitur yang bikin berat proses Edge
-  experimental: {
-    forceSwcTransforms: true,
-  },
+  basePath: '/web-desain', // Ini wajib ada
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
 }
+
 module.exports = nextConfig
