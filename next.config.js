c38134cbf365951fab1f1ba19a7b582eef02ec31
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-11 20:36:34 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Matikan fitur yang memicu munculnya manifest di Edge
+  swcMinify: true,
+  // Kita matikan fitur yang bikin berat proses Edge
   experimental: {
-    serverSourceMaps: false,
+    forceSwcTransforms: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', 
-      },
-    ],
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
 }
-
 module.exports = nextConfig
