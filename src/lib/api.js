4f7ff58fbe1010f57c9b7cb7c4edd9b45a407254
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-11 19:00:37 +0800

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,12 +1,24 @@
 const API_URL = process.env.NEXT_PUBLIC_WORDPRESS_API_URL;
 
 async function fetchAPI(query, { variables } = {}) {
+  // Tambahkan proteksi jika URL API lupa diisi
+  if (!API_URL) {
+    throw new Error('Environment variable NEXT_PUBLIC_WORDPRESS_API_URL is not defined');
+  }
+
   const res = await fetch(API_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ query, variables }),
   });
+
   const json = await res.json();
+
+  if (json.errors) {
+    console.error(json.errors);
+    throw new Error('Failed to fetch API');
+  }
+
   return json.data;
 }
 
@@ -27,13 +39,18 @@ export async function getWebDesignLandingData() {
 
 export async function getWebDesignPost(slug) {
   const data = await fetchAPI(`
-    query WebDesignBySlug($id: ID!) {
-      webDesign(id: $id, idType: SLUG) {
+    query WebDesignBySlug($id: ID!, $idType: WebDesignIdType!) {
+      webDesign(id: $id, idType: $idType) {
         title
         content
         date
       }
     }
-  `, { variables: { id: slug } });
+  `, { 
+    variables: { 
+      id: slug,
+      idType: 'SLUG' 
+    } 
+  });
   return data?.webDesign;
 }
