3eec5d52ea4996ce28a886dac8d162d429e654c6
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-11 20:30:56 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Tambahkan baris di bawah ini untuk mematikan optimasi yang bikin error
-  optimizeFonts: false,
-  productionBrowserSourceMaps: false,
+  // Matikan fitur yang memicu munculnya manifest di Edge
+  experimental: {
+    serverSourceMaps: false,
+  },
   images: {
     remotePatterns: [
       {
