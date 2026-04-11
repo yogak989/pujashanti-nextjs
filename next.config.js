20983e8f231bd508ce3bcab83f974249bdea9791
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-11 21:35:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/web-design', // Ini wajib ada
+  // Hapus baris basePath: '/web-design'
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
 }
-
 module.exports = nextConfig
