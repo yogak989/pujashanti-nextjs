91e2491514c6c6ae03274e366c118caa7c7e7869
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-11 21:00:00 +0800

    Update [slug].js

--- src/pages/web-design/[slug].js
+++ src/pages/web-design/[slug].js
@@ -1,4 +1,4 @@
-export const runtime = 'edge';
+export const runtime = 'experimental-edge';
 import { getWebDesignPost } from '../../lib/api';
 import TOC from '../../components/TOC';
 import Head from 'next/head';
