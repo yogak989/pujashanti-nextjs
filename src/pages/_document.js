d1b561c8aee4f1dafb4a53000a63fae52ac0b9c4
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-13 12:00:28 +0800

    Create _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    // Set lang="id" untuk Bahasa Indonesia
+    <Html lang="id">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
