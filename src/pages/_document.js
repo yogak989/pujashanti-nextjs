23657872d1208bd485f1bc7592e5f77807afb20b
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-16 20:31:36 +0800

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,9 +2,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    // Set lang="id" untuk Bahasa Indonesia
     <Html lang="id">
-      <Head />
+      <Head>
+        {/* Script Utama Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4663862602910608"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
