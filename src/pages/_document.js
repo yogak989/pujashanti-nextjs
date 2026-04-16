0e851637fd511bc2511f10193d3449857fdcd199
yogak989 <167546137+yogak989@users.noreply.github.com>
2026-04-17 06:44:32 +0800

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,7 @@ export default function Document() {
   return (
     <Html lang="id">
       <Head>
-        {/* Script Utama Google AdSense */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4663862602910608"
-          crossOrigin="anonymous"
-        ></script>
+        
       </Head>
       <body>
         <Main />
